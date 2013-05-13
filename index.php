7d21e70f1c9e85ef2713f141eed781983a84607c
tesav <tesav@yandex.ru>
2013-05-14 01:33:54 +0300

    1

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+define(R, 0);
+
+//  Главный входной файл
+
+if (version_compare(PHP_VERSION, '5.3.0', '<'))
+    die('PHP версия должна быть не меньше: [ 5.3.0 ] , а у вас: [ ' . phpversion() . ' ].');
+
+
+include 'action/common.php';
+include 'test/TestMonteCarlo.php';
+
+//
+new TestMonteCarlo;
