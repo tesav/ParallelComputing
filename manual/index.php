ac38811d8c20dfeb7312dadd305bad9b88fd635c
tesav <tesav@yandex.ru>
2013-05-14 00:15:05 +0300

    1
    Signed-off-by: tesav <tesav@yandex.ru>

---
+++ manual/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Инструкция</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>Подробнее в тестах..</p>
+        <ul>
+            <li><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/test/testBegotten.php" >Тест 1</a></li>
+            <li><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/test/testWorker.php" >Тест 2</a></li>
+            <li><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/test/testWorkerFunc.php" >Тест 3</a></li>
+        </ul>
+    </body>
+</html>
